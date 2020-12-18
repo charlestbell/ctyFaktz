c8f8204ce5acd84c9dbacc9f43da1d79fc76b6dc
C.T. Bell <charlestbell@gmail.com>
2020-12-17 22:21:01 -0500

    Updating Google API code

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,8 @@
+let heroku = "https://cors-anywhere.herokuapp.com/";
 let queryURL =
-  "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBxxikd5sBYySsC4ExQM_Y1plVzBP7Ljbk";
+  "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyBxxikd5sBYySsC4ExQM_Y1plVzBP7Ljbk";
 $.ajax({
-  url: queryURL,
+  url: heroku + queryURL,
   method: "GET",
   dataType: "json",
 }).then(function (response) {
