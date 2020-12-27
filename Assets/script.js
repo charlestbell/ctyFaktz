09a428d383d393f5a3036b8b04a879c562e61e77
Joseph <josephgrant91739@gmail.com>
2020-12-27 13:19:47 -0800

    nav bar css

--- Assets/script.js
+++ Assets/script.js
@@ -2,70 +2,6 @@ var searchInput = "";
 var terms;
 var population;
 var pop;
-var usedFacts = [];
-
-//an array of objects that holds the funny fact description and the number accosiated with it.
-var funFacts = [
-  {
-    description: "Kim's Convenience Stores",
-    amount: 100,
-  },
-  {
-    description: "Friendly neighborhood Spider men",
-    amount: 100,
-  },
-  {
-    description: "'IT' clowns hiding in storm drains",
-    amount: 100,
-  },
-  {
-    description: "Street rats",
-    amount: 100,
-  },
-  {
-    description: "Scenic overlooks for parking",
-    amount: 100,
-  },
-  {
-    description: "Sketchy street meat carts",
-    amount: 100,
-  },
-  {
-    description: "Disney Princesses",
-    amount: 100,
-  },
-  {
-    description: "Autobots in disguise",
-    amount: 100,
-  },
-  {
-    description: "Mutant Ninja Turtles in Sewers",
-    amount: 100,
-  },
-  {
-    description: "Mutants",
-    amount: 100,
-  },
-];
-
-//Random number genorator to get random number for funny facts
-//function also checks if random number has already been generated in current sequence
-function getrando() {
-  var rando = Math.floor(Math.random() * 10);
-  if (usedFacts[0] === null) {
-    //If used fact array is empty push random number to array
-    usedFacts.push(rando);
-  } else {
-    $.each(usedFacts, function (indexInArray) {
-      //Checks if random number exists in array
-      if (indexInArray === rando) {
-        getrando();
-      } //if number has already been called, get new number
-    });
-  }
-  return rando;
-}
-//beginning of Google maps API
 // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE&libraries=places">
 function initAutocomplete() {
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -73,21 +9,17 @@ function initAutocomplete() {
     zoom: 11, //zoom increases as number increases
     mapTypeId: "roadmap",
   });
-
   //Auto complete parameters which limits the search to cities
   // var options = {
   // types: ['(cities)'],
   // componentRestrictions: {country: "us"} //sets the search to a specific country
   //  };
-
   // Create the search box and link it to the UI element.
   const input = document.getElementById("pac-input");
   //Search box without auto correct
   const searchBox = new google.maps.places.SearchBox(input);
-
   //Search box with auto correct
   // const searchBox = new google.maps.places.Autocomplete(input, options);
-
   //attaches search bar to map
   // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
@@ -95,52 +27,36 @@ function initAutocomplete() {
   map.addListener("bounds_changed", () => {
     searchBox.setBounds(map.getBounds());
   });
-
   let markers = [];
-  // var usedFacts = [];  //////////////////////////////\\\\\\\\\\
-
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener("places_changed", () => {
-    $(".progress").removeClass("hide"); //Show Loading Bar
-
     const places = searchBox.getPlaces();
-
     if (places.length == 0) {
       return;
     }
-
-    // Clear out the old markers from map.
+    // Clear out the old markers.
     markers.forEach((marker) => {
       marker.setMap(null);
     });
-
     markers = [];
-    usedFacts = []; //Sets the array that contains the fun fact indexes that have bveen used to null or empty.
-
-    $("#cityDetails").empty(); //Empties out element that contains the city name and population
-    $("#citySummary").empty();
-
     // For each place, get the icon, name and location.
     const bounds = new google.maps.LatLngBounds();
+    /////////////////////////////////////
+    console.log(places);
 
     //create city name element
-    var cityName = $("<h4>")
-      .text(places[0].formatted_address)
-      .addClass("bold-text mt-1");
-
+    var cityName = $("<h1>").text(places[0].formatted_address);
     //append city name to name div
-    $("#cityDetails").append(cityName);
-
-    //Stores the user input in a variable for later use
+    $("#usefulFactoids").append(cityName);
+    // console.log(geonames[0].summary); //console log objects
     searchInput = places[0].name;
-
+    ///////////////////////
     places.forEach((place) => {
       if (!place.geometry) {
         console.log("Returned place contains no geometry");
         return;
       }
-
       const icon = {
         url: place.icon,
         size: new google.maps.Size(71, 71),
@@ -148,7 +64,6 @@ function initAutocomplete() {
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25),
       };
-
       // Create a marker for each place.
       markers.push(
         new google.maps.Marker({
@@ -166,58 +81,60 @@ function initAutocomplete() {
       }
     });
     map.fitBounds(bounds);
-
     //wikipedia api
     const geoSettings = {
       url:
         "http://api.geonames.org/wikipediaSearchJSON?q=" +
         searchInput +
         "&maxRows=10&username=hunter7",
-      // url: "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=hunter7",
-      // url: "http://api.geonames.org/getJSON?geonameId=8096217&username=hunter7",//this is good alot of info but still need the geoname id
-      // url: "http://api.geonames.org/wikipediaBoundingBoxJSON?north=34.033481&south=33.907677&east=-117.54897&west=-117.615501&username=hunter7", //this gets the summary
+      //   // url: "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=hunter7",
+      //   // url: "http://api.geonames.org/getJSON?geonameId=8096217&username=hunter7",//this is good alot of info but still need the geoname id
+      //   // url: "http://api.geonames.org/wikipediaBoundingBoxJSON?north=34.033481&south=33.907677&east=-117.54897&west=-117.615501&username=hunter7", //this gets the summary
       method: "GET",
     };
     $.ajax(geoSettings).then(function (resp) {
+      console.log("geonames");
+      console.log(resp);
       //create sumamry element
       var summary = $("<p>").text(resp.geonames[0].summary);
-      $("#citySummary").append(summary);
+      $("#usefulFactoids").append(summary);
     });
     //end of wiki api
+     terms = ["Starbucks", "Resturants", "Museums", "Bowling Alley"];
+
+    
+  // });
+// }
 
     //openweatherapi
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=55165c51eb244bc563baf90a2d02b714`;
+
+    var queryURL5Day = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=55165c51eb244bc563baf90a2d02b714`;
 
     //ajax call
     $.ajax({
-      url: queryURL,
+      url: queryURL5Day,
       method: "GET",
     }).then(function (response) {
+      console.log(response);
+      console.log(response.city.population);
       pop = response.city.population;
       population = $("<p>").text(`Population: ${response.city.population}`);
-      $("#cityDetails").append(population);
-
-      //Funny facts
-      for (var x = 0; x < 3; x++) {
-        var index = getrando(); //Gets random number that has not been called
-        var funFactAmounts = Math.round(pop / funFacts[index].amount);
-        var funnyFacts = funFacts[index].description; //gets fun fact description from array randomly
-        $(`#funFact${x}`).append(funnyFacts);
-        $(`#funAmount${x}`).append(funFactAmounts);
-      }
-      //end of funny facts
+      $("#usefulFactoids").append(population);
     });
-    //end os open weather api call
+    var queryURLcur = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=55165c51eb244bc563baf90a2d02b714`;
 
-    //Array which contains search parameters for yelp api call
-    terms = ["Starbucks", "resturant", "museum", "bowling alley"];
+    $.ajax({
+      url: queryURLcur,
+      method: "GET",
+    }).then(function (response) {
+      console.log(`weather`);
+      console.log(response);
+    });
+    //end of open weather
 
-    //For loop that calls the yelp api
-    //loop runs for each element in terms array
-    for (i = 0; i < terms.length; i++) {
+    for ( i = 0; i < terms.length; i++) {
       // yelp api
-      const nameOfResponse = terms[i];
-      const x = i;
+      const nameOfResponse = terms[i]
       var herokuApp = "https://cors-anywhere.herokuapp.com/";
       var settings = {
         async: true,
@@ -235,22 +152,41 @@ function initAutocomplete() {
             "Bearer vvvcCtcJU8SYfZfJOTtjqBoJWMEil9uycqIhOZ29UVDqHxQezCjKDUbSCkeAwmsrX4sRpOZffUZihfpWj08qre6NrhPhTeVzhBOhHjNcjDN2XHLYFvPVcgvDl5TZX3Yx",
           // "Access-Control-Allow-Origin": "*"
         },
+      
       };
-      $.ajax(settings).then(function (response) {
-        var perCapita = Math.ceil(pop / response.total);
 
-        var items =
-          "There is 1 " + nameOfResponse + " per " + perCapita + " people.";
-        $(`#usefulFactoids${x}`).empty();
-        $(`#usefulFactoids${x}`).append(items);
-        $(".progress").addClass("hide"); //Hide Loading Bar
+      $.ajax(settings).then(function (response) {
+        // console.log(nameOfResponse);
+        // console.log("total" + response.total);
+        // console.log("population" + pop);
+        var perCapita = (response.total / pop) ;
+        var items = $("<p>").text(nameOfResponse+ " per capita: " + perCapita);
+        $("#usefulFactoids").append(items);
       });
     }
   });
 }
 
-//Google places api | Need to look into this to see if we can pull pictures from it
-//Currently not in use
+//data api
+
+//     var newtag = $("<h1>").text(resp.geonames[0].summary);
+//         $("h1").append(newtag);
+
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://world-geo-data.p.rapidapi.com/cities/8096217",
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-key": "7d0f311c39msh58381986eafbd8fp19ed1djsnfeb535bffdf7",
+//     "x-rapidapi-host": "world-geo-data.p.rapidapi.com",
+//   },
+// };
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+//   var newtag = $("<h1>").text(response.population);
+//   $("h1").append(newtag);
+// });
 let heroku = "https://cors-anywhere.herokuapp.com/";
 let queryURL =
   "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=restaurants+in+sydney&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyBxxikd5sBYySsC4ExQM_Y1plVzBP7Ljbk";
@@ -258,4 +194,9 @@ $.ajax({
   url: heroku + queryURL,
   method: "GET",
   dataType: "json",
-}).then(function (response) {});
+}).then(function (response) {
+  //   console.log("places sydney");
+  //   console.log(response);
+});
+
+
